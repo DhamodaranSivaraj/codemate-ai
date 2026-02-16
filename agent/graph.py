1da9407075703b1566b40a335553884752e867ab
DhamodaranSivaraj <dhamodaransivaraj04@gmail.com>
2026-02-16 13:35:36 +0530

    Initial clean commit

---
+++ agent/graph.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_groq import ChatGroq
+from prompts import *
+from states import *
+
+llm = ChatGroq(model="openai/gpt-oss-120b")
+
+
+
+user_prompt = "create a simple calculator web application"
+
+prompt = planner_prompt(user_prompt)
+
+
+
+resp = llm.with_structured_output(Plan).invoke(prompt)
+
+print(resp)
