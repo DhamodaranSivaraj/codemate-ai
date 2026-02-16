5d0f3b25731576696025a3c80f11787fac269f08
DhamodaranSivaraj <dhamodaransivaraj04@gmail.com>
2026-02-16 19:34:50 +0530

    spell check

--- agent/states.py
+++ agent/states.py
@@ -8,5 +8,5 @@ class Plan(BaseModel):
     name: str = Field(description="The name of app to be built")
     description: str = Field(description="A oneline description of the app to be built, e.g. 'A web application for managing personal finances'" )
     techstack: str = Field(description="The tech stack is used for the app, e.g. 'python', 'javascript', 'react', 'flask', etc.'")
-    featues: list[str] = Field(description="A list of features that the app should have, e.g. 'user authentication', 'data visualization', etc.'")
+    features: list[str] = Field(description="A list of features that the app should have, e.g. 'user authentication', 'data visualization', etc.'")
     files: list[File] = Field(description="A list of files to be created, each with a 'path' and 'purpose'")
